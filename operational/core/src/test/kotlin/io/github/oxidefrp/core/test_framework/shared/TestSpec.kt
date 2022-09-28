2237601a9813317ab0e586d4952e2332ba88001b
cubuspl42 <cubuspl42@gmail.com>
2022-09-28 20:10:27 +0200

    Symlink `shared` packages

--- operational/core/src/test/kotlin/io/github/oxidefrp/core/test_framework/shared/TestSpec.kt
+++
@@ -1,9 +0,0 @@
-package io.github.oxidefrp.core.test_framework.shared
-
-internal interface TestCheckGroup {
-    val checks: List<TestCheck<*>>
-}
-
-internal data class TestSpec(
-    override val checks: List<TestCheck<*>>,
-) : TestCheckGroup
--- operational/core/src/test/kotlin/io/github/oxidefrp/core/test_framework/shared/utils.kt
+++
@@ -1,4 +0,0 @@
-package io.github.oxidefrp.core.test_framework.shared
-
-inline fun <E, R : Comparable<R>> List<E>.isMonotonicallyIncreasingBy(crossinline selector: (E) -> R): Boolean =
-    zipWithNext { a, b -> selector(a) < selector(b) }.none { isIncreasing -> !isIncreasing }
