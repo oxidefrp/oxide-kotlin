27dfe0a7267418d38f0804a1f52dd20130e14854
cubuspl42 <cubuspl42@gmail.com>
2022-09-30 17:27:28 +0200

    Implement operational `buildInputSignal`

--- operational/core/src/test/kotlin/io/github/oxidefrp/core/test_framework/TestContext.kt
+++ operational/core/src/test/kotlin/io/github/oxidefrp/core/test_framework/TestContext.kt
@@ -2,16 +2,21 @@ package io.github.oxidefrp.core.test_framework
 
 import io.github.oxidefrp.core.Cell
 import io.github.oxidefrp.core.EventStream
+import io.github.oxidefrp.core.Signal
 import io.github.oxidefrp.core.impl.event_stream.CellVertex
 import io.github.oxidefrp.core.impl.event_stream.EventStreamVertex
+import io.github.oxidefrp.core.impl.signal.SignalVertex
 import io.github.oxidefrp.core.test_framework.input.InputCellVertex
 import io.github.oxidefrp.core.test_framework.input.InputEventStreamVertex
+import io.github.oxidefrp.core.test_framework.input.InputSignalVertex
 import io.github.oxidefrp.core.test_framework.shared.CellValueSpec
 import io.github.oxidefrp.core.test_framework.shared.EventOccurrenceDesc
 import io.github.oxidefrp.core.test_framework.shared.FiniteInputCellSpec
 import io.github.oxidefrp.core.test_framework.shared.FiniteInputStreamSpec
 import io.github.oxidefrp.core.test_framework.shared.InputCellSpec
+import io.github.oxidefrp.core.test_framework.shared.InputSignalSpec
 import io.github.oxidefrp.core.test_framework.shared.InputStreamSpec
+import io.github.oxidefrp.core.test_framework.shared.Tick
 
 internal class TestContext(
     private val tickStream: TickStream,
@@ -50,5 +55,20 @@ internal class TestContext(
             initialValue = initialValue,
             innerValues = innerValues,
         ),
+    )
+
+    fun <A> buildInputSignal(
+        spec: InputSignalSpec<A>,
+    ): Signal<A> = object : Signal<A>() {
+        override val vertex: SignalVertex<A> = InputSignalVertex(
+            tickStream = tickStream,
+            spec = spec,
+        )
+    }
+
+    fun <A> buildInputSignal(
+        provideValue: (tick: Tick) -> A,
+    ): Signal<A> = buildInputSignal(
+        spec = InputSignalSpec(provideValue = provideValue)
     )
 }
